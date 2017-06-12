5bae24f05b4e560541b1bdea3874340a16e4b1c8
Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-06-13 00:42:40 +0300

    HW9 added

--- HW9/js/script.js
+++ HW9/js/script.js
@@ -53,29 +53,30 @@ let developer2 = {
     ],
     requires: ['java', 'json', 'c++', 'JavaScript'],
     goodDev: goodDev,
-    getArr: getArr,
+    // getArr() {
+    //     console.log(this.experience);
+    //     // return this.experience.map(elem => elem.technology);
+    // }
 };
-
-function getArr() {
-    return this.experience.map(elem => elem.technology);
-}
 
 function goodDev(dev) {
     this.requires.forEach(elem => {
-        if (
-            dev.getArr.call(developer2).includes(elem) || dev.skills.includes(elem)
-        ) {
-            return `required: ${elem} ...sucsess`;
-        } else {
-            return `required: ${elem} ...fail`;
-        }
+        // console.log(dev.skills);
+        // console.log(elem);
+        // if (
+        //     dev.skills.includes(elem)
+        // ) {
+        //     return `required: ${elem} ...sucsess`;
+        // } else {
+        //     return `required: ${elem} ...fail`;
+        // }
     });
 }
 
 let developers = [developer1, developer2];
 developers.forEach((dev, index) => {
     console.log(`developer ${index + 1}`);
-    dev.goodDev();
+    dev.goodDev(dev);
 });
 // developer 1
 // required: Node.js ... success
@@ -88,6 +89,42 @@ developers.forEach((dev, index) => {
 // required: JavaScript ... success
 // required: Java ... success
 // required: OOP ... success
+/*
+ *
+ * TASK 2
+ *
+ * Создайте объект содержащий коллекцию элементов с одинаковыми
+ * свойстами и разными значениями!
+ * Напишите функцию принимает 1 аргумент сортирует объект по 
+ * переданному значению
+ * 
+ * При вызове функции используйте this
+ *
+ * */
+
+let myObject = {
+    database: [{ age: 100, name: 'b' }, { age: 15, name: 'c' }, { age: 25, name: 'a' }],
+    myFilter: myFilter,
+};
+
+function myFilter(key) {
+    this.database.sort((param1, param2) => {
+        if (param1[key] > param2[key]) {
+            return 1;
+        } else if (param1[key] < param2[key]) {
+            return -1;
+        }
+    });
+    return this.database;
+}
+// {age:15, name:'c'}, {age:25, name:'a'} {age:100, name:'b'}
+
+console.log(myObject.myFilter('age')); // when i comment someone console.log it works correctly...
+//Why?
+console.log(myObject.myFilter('name'));
+
+// {age:25, name:a}, {age:100, name: b} ...
+
 /*
  1. Переместите 0 в конец массива, остальные числа должны остаться
  неизменными
@@ -123,20 +160,15 @@ let arr1 = [
     9,
 ];
 
-// function moveZeroToEnd(arr) {
-//     let tempArr = [];
-//     let elem = [];
-//     let newArr = [];
-//     for (let i = 0; i < arr.length; i++) {
-//         if (arr[i] !== 0) {
-//             newArr.push(arr[i]);
-//         } else {
-//             newArr.push(arr[i]);
-//         }
-//     }
-//     return newArr.concat(newArr);
-// }
-// console.log(moveZeroToEnd(arr1));
+let moveZeroToEnd = arr => {
+    let tempArr = [];
+    let newArr = [];
+    arr.forEach(elem => {
+        elem === 0 ? newArr.push(elem) : tempArr.push(elem);
+    });
+    return tempArr.concat(newArr);
+};
+console.log(moveZeroToEnd(arr1));
 // /*
 //  2. Верните сумму двух найменьших чисел в массиве
 //
@@ -146,27 +178,20 @@ let arr1 = [
 //  [0,200,10,25,15] => 10
 //
 //  */
-// // let arr = [0, 200, 10, 25, 15];
-//     function compareNumeric(a, b) {
-//         if (a > b) return 1;
-//         if (a < b) return -1;
-//     }
-// let newArrSort =arr.sort(compareNumeric);
-//     function minimalNumber(arr) {
-//         return arr[0] + arr[1];
-//     }
-// console.log(minimalNumber(newArrSort));
+let arr = [0, 200, 10, 25, 15];
+let minimalNumber = arr => {
+    let newArrSort = arr.sort((a, b) => a - b);
+    return newArrSort[0] + newArrSort[1];
+};
+console.log(minimalNumber(arr));
 // // /*
 // //  3. Напишите функцию которая меняет местами имя и фамилию
 // //  */
-// function nameShuffler(string) {
-//     let arr = string.split(' ').reverse();
-//     let newName = arr.join(' ');
-//     return newName;
-// }
-// console.log(nameShuffler('john McClane'));
-// console.log(nameShuffler('Arnold Schwarzenegger'));
-// console.log(nameShuffler('James Bond'));
+let nameShuffler = string => string.split(' ').reverse().join(' ');
+
+console.log(nameShuffler('john McClane'));
+console.log(nameShuffler('Arnold Schwarzenegger'));
+console.log(nameShuffler('James Bond'));
 // // /*
 // //  // !
 // //  4. Напишите функцию которая принимает массив с именами и возвращает массив
@@ -175,20 +200,10 @@ let arr1 = [
 // //  capMe(['KARLY', 'DANIEL', 'KELSEY']) // returns ['Karly', 'Daniel', 'Kelsey']
 // //  */
 // //
-// function getName(arrName) {
-//     let name;
-//     let newName = [];
-//     for (let i = 0; i < arrName.length; i++) {
-//         arrName[i] = arrName[i].toLowerCase();
-//         name = arrName[i].split('');
-//         name[0] = name[0].toUpperCase();
-//         name = name.join('');
-//         newName.push(name);
-//     }
-//     return newName;
-// }
-// console.log(getName(['jo', 'nelson', 'jurie']));
-// console.log(getName(['KARLY', 'DANIEL', 'KELSEY']));
+let getName = arrName =>
+    arrName.map(elem => elem[0].toUpperCase() + elem.slice(1).toLowerCase());
+console.log(getName(['jo', 'nelson', 'jurie']));
+console.log(getName(['KARLY', 'DANIEL', 'KELSEY']));
 //@SUPER
 /*
  1. Найдите число отсутствующее в заданной последовательности
@@ -200,12 +215,15 @@ let arr1 = [
  [0,16,24,32] => 8
 
  */
-// function findNum(arrStep) {
-//     let a = arrStep[0];
-//     let b = arrStep[arrStep.length - 1];
-//     let c = arrStep.length;
-//     let step = (b - a) / c;
-
-//     console.log(step);
-// }
-// console.log(findNum([1, 3, 5, 9]));
+let findNum = arrStep => {
+    let step = (arrStep[arrStep.length - 1] - arrStep[0]) / arrStep.length;
+    let arr = arrStep.reduce((preval, val, i, arr) => {
+        if (preval + step === val) {
+            console.log((preval += step));
+        } else {
+            console.log((preval += step));
+        }
+        return preval;
+    });
+};
+console.log(findNum([1, 3, 5, 9]));
